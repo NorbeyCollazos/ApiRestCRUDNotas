67512417151f91adb5932ede2f793597eb8846a7
Norbey <ncrcollazos@gmail.com>
2021-08-21 21:10:58 -0500

    inicio del proyecto y estructura

---
+++ app/src/main/java/com/ncrdesarrollo/apirestcrudnotas/fragments/ListaNotasFragment.java
@@ -0,0 +1,33 @@
+package com.ncrdesarrollo.apirestcrudnotas.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ncrdesarrollo.apirestcrudnotas.R;
+
+public class ListaNotasFragment extends Fragment {
+
+
+    public ListaNotasFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lista_notas, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/ncrdesarrollo/apirestcrudnotas/fragments/RegistrarNotaFragment.java
@@ -0,0 +1,34 @@
+package com.ncrdesarrollo.apirestcrudnotas.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ncrdesarrollo.apirestcrudnotas.R;
+
+public class RegistrarNotaFragment extends Fragment {
+
+
+    public RegistrarNotaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registrar_nota, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
